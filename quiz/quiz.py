84f1556cef3ef8c3a09c5d1a03bc0125a7b22d9a
dobrawarog <dobrawa.rog@lo1.sandomierz.pl>
2020-05-23 16:24:34 +0200

    quiz

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -4,13 +4,48 @@
 
 from flask import Flask
 from flask import render_template
+from flask import request, redirect, url_for, flash
 
 app = Flask(__name__)
+# konfiguracja aplikacji
+app.config.update(dict(
+    SECRET_KEY='bradzosekretnawartosc',
+))
 
-@app.route('/')
+# lista pytań
+DANE = [{
+    'pytanie': 'Stolica Hiszpani, to:',  # pytanie
+    'odpowiedzi': ['Madryt', 'Warszawa', 'Barcelona'],  # możliwe odpowiedzi
+    'odpok': 'Madryt'},  # poprawna odpowiedź
+    {
+    'pytanie': 'Objętość sześcianu o boku 6 cm, wynosi:',
+    'odpowiedzi': ['36', '216', '18'],
+    'odpok': '216'},
+    {
+    'pytanie': 'Symbol pierwiastka Helu, to:',
+    'odpowiedzi': ['Fe', 'H', 'He'],
+    'odpok': 'He'},
+    {
+    'pytanie': 'Jak nazywa się najgłębsze jezioro świata?:',
+    'odpowiedzi': ['Bajkał', 'Czad', 'Michigan'],
+    'odpok': 'Bajkał'},
+]
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    #return 'Cześć, tu Python!'
-    return render_template('index.html')
+
+    if request.method == 'POST':
+        punkty = 0
+        odpowiedzi = request.form
+
+        for pnr, odp in odpowiedzi.items():
+            if odp == DANE[int(pnr)]['odpok']:
+                punkty += 1
+
+        flash('Liczba poprawnych odpowiedzi, to: {0}'.format(punkty))
+        return '<h1>Poprawne odpowiedzi: <u>'+str(punkty)+'</u></h1>'
+
+    # return 'Cześć, tu Python!'
+    return render_template('index.html', pytania=DANE)
 
 if __name__ == '__main__':
     app.run(debug=True)
